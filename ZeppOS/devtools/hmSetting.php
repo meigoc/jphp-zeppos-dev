342f25ef90da40cf916442d75c610ba47a025b10
Meigo <73817505+meigoc@users.noreply.github.com>
2024-09-24 01:10:04 +0300

    Create hmSetting.php

---
+++ ZeppOS/devtools/hmSetting.php
@@ -0,0 +1,8 @@
+<?php
+
+public $width = null; // width of the device screen // Type: Number
+public $height = null; // device screen height // Type: Number
+public $screenShape = null; // Screen shape, 0-square screen, 1-round screen // Type: Number
+public $deviceName = null; // device name // Type: string
+public $keyNumber = null; // number of keys // Type: Number
+public $deviceSource = null; // device code // Type: Number
